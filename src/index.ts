9e9807fbae6f80862a0459bcdb7ddeb77885ca69
rash <orashusedmund@gmail.com>
2023-10-14 14:39:23 +0100

    also add ejs and tookoff marko.js

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import App from "./services/express/express.app";
 (async () => {
     const app: Application = express();
 
-    const server_message = `listening on \nhttp://localhost:${PORT}/ \n\n`
+    const server_message = `listening on \nhttp://localhost:${PORT}/ \n\n`;
 
     App(app)
         .then(() => app.listen(PORT, function () {
