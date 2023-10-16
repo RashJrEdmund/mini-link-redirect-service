d1bf0fcde1a36d0769bd702ef37e9c37dcc0152a
rash <orashusedmund@gmail.com>
2023-10-16 11:05:33 +0100

    added the landing route for get url/r

--- src/routes/url/index.ts
+++ src/routes/url/index.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Response } from "express";
 import { EPXRESS_URL_CONTROLLER } from "../../modules";
+import { FRONT_END_URL } from "../../services/constants";
 
 const router = express.Router();
 
 const url_controller = new EPXRESS_URL_CONTROLLER();
+
+router.get('/', (_, res: Response) => {
+    res.render("landing", { title: "Mini - Link", FRONT_END_URL });
+});
 
 router.get('/:short_link', url_controller.redirect_to_url.bind(url_controller));
 
